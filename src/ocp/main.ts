444383a4d75666417a911ec3d8db4fa4ae5174a8
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-08-01 18:18:53 -0300

    Open/closed principle applied

---
+++ src/ocp/main.ts
@@ -0,0 +1,27 @@
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+//const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+shoppingCart.addItem(new Product('Shirt', 100.98));
+shoppingCart.addItem(new Product('Book', 20.52));
+shoppingCart.addItem(new Product('Pen', 8.44));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+
+console.log(order.orderStatus);
+
+order.checkout();
+console.log(order.orderStatus);
